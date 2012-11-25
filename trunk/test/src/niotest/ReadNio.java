e77f004c3d6d7f03604b5315d96c8822df56198b
visualkhh@gmail.com <visualkhh@gmail.com@9f7e3b54-5923-cb47-458c-14d5223930df>
2012-11-25 06:22:36 +0000

    f

--- trunk/test/src/niotest/ReadNio.java
+++ trunk/test/src/niotest/ReadNio.java
@@ -2,6 +2,7 @@ package niotest;
 
 import khh.communication.tcp.nio.server.NioServer;
 
+
 public class ReadNio {
     public static void main(String[] args) throws Exception {
         NioServer a = new NioServer(8080,ReadWorker.class);
--- trunk/test/src/niotest/ReadWorker.java
+++ trunk/test/src/niotest/ReadWorker.java
@@ -8,15 +8,17 @@ import khh.debug.LogK;
 
 public class ReadWorker extends NioWorker {
     LogK log = LogK.getInstance();
-public ReadWorker() {
-    // TODO Auto-generated constructor stub
-}
+
+    public ReadWorker() {
+        setFirestMode(MODE_FIREST_R);
+    }
     @Override
     public void execute(SelectionKey selectionKey) throws Exception {
         ByteBuffer b = ByteBuffer.allocate(1);
         if ( selectionKey.isReadable() ) {
             read(b, 100);
-            System.out.print(b.getChar());
+            b.clear();
+            System.out.print((char)b.get());
         }
 
     }
