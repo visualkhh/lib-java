a226a2d86bed40a6648931da085244552dc3d0e6
visualkhh@gmail.com <visualkhh@gmail.com@9f7e3b54-5923-cb47-458c-14d5223930df>
2012-12-07 19:42:44 +0000

    f

--- trunk/Utilkhh/src/khh/communication/tcp/nio/util/NioUtil.java
+++ trunk/Utilkhh/src/khh/communication/tcp/nio/util/NioUtil.java
@@ -2,6 +2,8 @@ package khh.communication.tcp.nio.util;
 
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 
 public class NioUtil {
@@ -9,4 +11,13 @@ public class NioUtil {
 	public static Iterator<SelectionKey> getSelectionKeys(Selector selector){
 		return selector.selectedKeys().iterator();
 	}
+	public static SocketChannel getSocketChannel(SelectionKey selectionKey){
+	       SocketChannel channel = (SocketChannel)selectionKey.channel();
+	       return channel;
+	   }
+	public static ServerSocketChannel getServerSocketChannel(SelectionKey selectionKey){
+	    ServerSocketChannel channel = (ServerSocketChannel)selectionKey.channel();
+	       return channel;
+	   }
+	   
 }
--- trunk/Utilkhh/src/khh/communication/tcp/nio/worker/NioWorker.java
+++ trunk/Utilkhh/src/khh/communication/tcp/nio/worker/NioWorker.java
@@ -3,15 +3,18 @@ package khh.communication.tcp.nio.worker;
 import java.io.IOException;
 import java.net.SocketTimeoutException;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 
 import khh.communication.Communication_Interface;
 import khh.communication.Connection_Interface;
+import khh.communication.tcp.nio.protocol.NioMsg;
 import khh.communication.tcp.nio.server.NioServer;
 import khh.debug.LogK;
 import khh.debug.util.DebugUtil;
 import khh.std.adapter.Adapter_Base;
+import khh.util.ByteUtil;
 import khh.util.Util;
 
 abstract public class NioWorker{
@@ -24,6 +27,14 @@ abstract public class NioWorker{
 	private SocketChannel socketChannel			= null;
 	private LogK log 							= LogK.getInstance();
 	private NioServer server =  null;
+	
+	
+	public NioWorker(int firestMode) {
+	    setFirestMode(firestMode);
+    }
+	public NioWorker() {
+    }
+	
 	final synchronized public void write(String data) throws IOException{
 		write(data.getBytes());
 	}
@@ -44,11 +55,15 @@ abstract public class NioWorker{
 	final synchronized public int write(ByteBuffer data,SocketChannel socketChannel) throws IOException{
 		int write_length=0;
 		try{
-//			log.debug("1) IsConnected  "+isConnected());
+//		    System.out.println( log.toHexlog(data));
+//		    DebugUtil.trace(ByteUtil.toByteArray(data), "---------");
+//		    System.out.println( log.toHexlog(data));
+			//log.debug("Write Before  "+isConnected(),data);
 			write_length = socketChannel.write(data);
+			//log.debug("Write after OK!  "+isConnected());
 //			log.debug("2) WriteLength "+write_length);
 		}catch(IOException e){
-			log.debug("3) Exception : Write Fail Socket=null  isConnected? -> "+isConnected());
+			log.debug("Exception : Write Fail Socket=null  isConnected? -> "+isConnected());
 			throw e;
 		}
 		return write_length;
@@ -91,6 +106,101 @@ abstract public class NioWorker{
 			}
 		}
 	}	
+	
+	
+	
+	
+	//////////protocol!!
+	   /*
+     * STX(1byte), ACTIONCODE(int4byte), LENGTH(datalength int4byte), DATA(bytes..~..), ETX(1byte) 
+     */
+	public NioMsg receiveNioMsg() throws IOException {
+	    return receiveNioMsg(getSocketChannel());
+	}
+	public NioMsg receiveNioMsg(SelectionKey selectionKey) throws IOException {
+	    return receiveNioMsg((SocketChannel)selectionKey.channel());
+	}
+    public NioMsg receiveNioMsg(SocketChannel socket) throws IOException {
+        NioMsg msg = null;
+//        if(selectionKey.isReadable()){
+            ByteBuffer buffer = ByteBuffer.allocate(1);
+            int i = read(buffer, 1000,socket); //stx Read
+            buffer.clear();
+            //log.debug("SEX",buffer);
+            
+            if(i==1 && NioMsg.STX == buffer.get(0)){
+                msg = new NioMsg();
+                //msg.setStx(buffer.get(0));
+                
+                buffer = ByteBuffer.allocate(4);
+                buffer.order(ByteOrder.BIG_ENDIAN);
+                i = read(buffer, 1000,socket);         //action Code Read
+                buffer.clear();
+                int actionCode = buffer.getInt();
+                buffer.clear();
+                //log.debug("ACTION("+actionCode+")",buffer);
+                msg.setAction(actionCode);
+                
+                
+                buffer = ByteBuffer.allocate(4);    //length Code Read
+                buffer.order(ByteOrder.BIG_ENDIAN);
+                i = read(buffer, 1000,socket);
+                buffer.clear();
+                int length = buffer.getInt();
+                buffer.clear();
+               // log.debug("LENGTH("+length+")",buffer);
+//              msg.setAction(length);
+                
+                buffer = ByteBuffer.allocate(length);//data Read
+                i = read(buffer, 2000,socket);
+                buffer.clear();
+                msg.set(buffer);
+                //log.debug("DATA",buffer);
+                
+                buffer = ByteBuffer.allocate(1);        //etx Read
+                i = read(buffer, 1000,socket);
+                buffer.clear();
+                //msg.setEtx(buffer.get(0));
+               // log.debug("ETX",buffer);
+                
+                if(i==1 && NioMsg.ETX ==buffer.get(0)){
+                //    log.debug("Good Message ActionCode :"+ msg.getAction());
+                    msg.setSuccess(true);
+                    //msg.setSelectionKey(selectionKey);
+                }
+            }
+//        }
+        return msg;
+    }
+    
+    
+    public void sendNioMsg(NioMsg msg) throws Exception {
+        sendNioMsg(msg,getSocketChannel());
+    }
+    public void sendNioMsg(NioMsg msg, SelectionKey selectionKey) throws Exception {
+        sendNioMsg(msg,(SocketChannel)selectionKey.channel());
+    }
+    public void sendNioMsg(NioMsg msg, SocketChannel socket) throws Exception {
+        if(msg.isSuccess()){
+            write(msg.unformat(),socket);
+        }
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	final public boolean isConnected(){
 		if ( getSocketChannel() == null )
 			return false;
