f40a045c6eeb5fb279203effac480183867e1aff
visualkhh@gmail.com <visualkhh@gmail.com@9f7e3b54-5923-cb47-458c-14d5223930df>
2012-12-07 16:22:28 +0000

    f

--- trunk/Utilkhh/src/khh/format/Formater.java
+++ trunk/Utilkhh/src/khh/format/Formater.java
@@ -1,10 +1,13 @@
 package khh.format;
 
+import java.nio.ByteBuffer;
+
 import khh.cast.CastBase;
 
 
 abstract public class  Formater<T> extends CastBase<T> {
 	
-	public abstract void format(T data);
 
+	public abstract void format(T data);
+	public abstract T unformat();
 }
