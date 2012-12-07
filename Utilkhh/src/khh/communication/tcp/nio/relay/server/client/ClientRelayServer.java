7d76d5c9c8a73706d64a701ef6cf4d695274e0f2
visualkhh <visualkhh@gmail.com>
2012-12-07 16:21:42 +0000

    f

--- Utilkhh/src/khh/communication/tcp/nio/relay/server/client/ClientRelayServer.java
+++ Utilkhh/src/khh/communication/tcp/nio/relay/server/client/ClientRelayServer.java
@@ -7,23 +7,8 @@ public class ClientRelayServer extends NioServer{
 	public ClientRelayServer(int port,Class nioWorkerClass){
 		super(port,nioWorkerClass);
 	}
-	
-
-	
-
 	public void start() throws Exception{
-//		server = new NioServer(getPort(), ClientRelayServerWorker.class);
-//		server.setName("ClientRelayServer"+getPort());
-//		server.setSelectorPoolSize(getSelectorPoolSize());
-//		server.setWorkerPoolSize(getWorkerPoolSize());
-//		server.start();
 		super.start();
 		getMultimonitor().addMonitor(getName(),getMonitor());
 	}
-
-	
-	
-
-
-
 }
