728b97c5027d72c2d0aa12fbfd00c9565f303bf5
visualkhh <visualkhh@gmail.com>
2012-12-07 06:55:38 +0000

    f

--- RemoteManager/src/khh/project/remote/remotemanager/admin/AdminServer.java
+++ RemoteManager/src/khh/project/remote/remotemanager/admin/AdminServer.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 import khh.communication.tcp.nio.server.NioServer;
 import khh.communication.tcp.nio.worker.NioWorker;
+import khh.project.remote.remotemanager.monitor.RemoteServerMonitor;
 import khh.project.remote.remotemanager.msg.RemoteMsg;
 import khh.std.adapter.Adapter_Std;
 
@@ -24,6 +25,7 @@ public class AdminServer{
 		server.setSelectorPoolSize(10);
 		server.setWorkerPoolSize(10);
 		server.start();
+		RemoteServerMonitor.getInstance().addAdminMinitor(server.getMonitor());
 	}
 
 
