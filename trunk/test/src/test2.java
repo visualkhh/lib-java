cefe9da3deb0064c707c673e41bedd90059f300d
visualkhh@gmail.com <visualkhh@gmail.com@9f7e3b54-5923-cb47-458c-14d5223930df>
2012-12-07 06:55:54 +0000

    f

--- trunk/test/src/test2.java
+++ trunk/test/src/test2.java
@@ -1,18 +1,16 @@
-import com.kdn.util.conversion.ConversionUtil;
+import java.util.HashMap;
+
 
 public class test2 {
 
     public static void main(String[] args) {
 
-        String aa =null;
-        String aab ="";
-        System.out.println(aa);
-        System.out.println(aab);
-        //Integer.parseInt(aa);    
-        
-        String input=null;
-            String a = ConversionUtil.nullToString(input,"avava");
-            System.out.println(a);
-            
+    	String[] a = {"aa","bb","cc","dd"};
+    	System.out.println(a.toString());
+            HashMap<String,String> ab = new HashMap<String,String>();
+            ab.put("a", "b");
+            ab.put("b", "b");
+            ab.put("c", "b");
+            System.out.println(ab.toString());
     }
 }
