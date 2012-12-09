30663f609045cfb4bc818d5b9d7b073361b84086
visualkhh@gmail.com <visualkhh@gmail.com@9f7e3b54-5923-cb47-458c-14d5223930df>
2012-12-09 22:32:55 +0000

    h

--- trunk/Utilkhh/src/khh/sort/SortUtil.java
+++ trunk/Utilkhh/src/khh/sort/SortUtil.java
@@ -4,8 +4,13 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import khh.sort.comparator.CompareBase;
+
 public class SortUtil {
     public static void sort(List list,Comparator comparator){
+        Collections.sort(list,comparator);
+    }
+    public static void sort(List list,CompareBase comparator){
         Collections.sort(list,comparator);
     }
 }
