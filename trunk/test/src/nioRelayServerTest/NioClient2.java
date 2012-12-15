64900523996c565106acfce7ee0e68b77a9c671c
visualkhh@gmail.com <visualkhh@gmail.com@9f7e3b54-5923-cb47-458c-14d5223930df>
2012-12-15 04:32:32 +0000

    h

--- trunk/test/src/nioRelayServerTest/NioClient2.java
+++ trunk/test/src/nioRelayServerTest/NioClient2.java
@@ -1,51 +1,52 @@
 package nioRelayServerTest;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 
 import khh.communication.tcp.nio.client.NioClient;
 import khh.communication.tcp.nio.worker.NioActionWorker;
 import khh.communication.tcp.nio.worker.NioWorker;
-import khh.communication.tcp.nio.worker.msg.NioParamMsg;
 import khh.communication.tcp.nio.worker.msg.NioActionMsg;
 import khh.debug.LogK;
-import khh.util.ByteUtil;
 
 public class NioClient2 {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
 
     	NioActionWorker client1Worker = new NioActionWorker(NioWorker.MODE_FIREST_W) {
             LogK log = LogK.getInstance();
-
 			@Override
-			public NioActionMsg onReceiveAction(NioActionMsg msg, SelectionKey selectionKey)throws Exception{
-                NioParamMsg fromto = new NioParamMsg();
-                fromto.format(msg.get());
-                log.debug("Recv Msg :("+msg.getAction()+") " + fromto.getString());
-                return msg;
-			}
-
-			@Override
-			public NioActionMsg onSendAction(NioActionMsg msg, SelectionKey selectionKey)throws Exception{
-              NioParamMsg fromto = new NioParamMsg();
-              fromto.setFrom("client2");
-              fromto.setTo("client1");
-              fromto.set(ByteUtil.toByteBuffer("From:Client2(801) ~ To: Clinet1(9090)   Msg: hi~client1"));
-              ByteBuffer data = fromto.unformat();
-
-              msg = new NioActionMsg();
-              msg.set(data);
-              msg.setAction(ACTION.FROMTO.getValue());
-              msg.setSuccess(true);
-              sendNioActionMsg(msg);
-              Thread.sleep(3000);
+			public NioActionMsg onReceiveAction(NioActionMsg msg, SelectionKey selectionKey) throws Exception{
+              log.debug("Recv Msg :("+msg.getAction()+") " + msg.getString());
               return msg;
 			}
-        };
-        NioClient client2 = new NioClient("127.0.0.1", 801, client1Worker);
-        client2.connection();
-        client2.start();
+			
+            @Override
+            public NioActionMsg onSendAction(NioActionMsg msg, SelectionKey selectionKey) throws Exception {
+                if(msg==null){
+                    msg=new NioActionMsg();
+                }
+                msg.clear();
+                 msg.setAction(NioActionMsg.ACTION.ECHO.getValue());
+//                 msg.setAction(NioActionMsg.ACTION.GET_SERVERTIME.getValue());
+                msg.setAction(NioActionMsg.ACTION.FROMTO.getValue());
+              //  msg.setAction(NioActionMsg.ACTION.GET_SERVERS.getValue());
+             //   msg.setAction(NioActionMsg.ACTION.GET_CLIENTS.getValue());
+                msg.putParam(NioActionMsg.PARAM.FROM.getValue(), "client2");
+                msg.putParam(NioActionMsg.PARAM.TO.getValue(), "client1");
+                // fromto.set(ByteUtil.toByteBuffer("From:Client1(9090) ~ To: Clinet2(801)   Msg: hi~client2"));
+//                msg.set("ClientRelayServer9090");
+                msg.set("hey~Client1");
+                msg.setSuccess(true);
 
+                sendNioActionMsg(msg);
+                Thread.sleep(3000);
+
+                return msg;
+            }
+
+        };
+        NioClient client1 = new NioClient("127.0.0.1", 801, client1Worker);
+        client1.connection();
+        client1.start();
     }
 }
