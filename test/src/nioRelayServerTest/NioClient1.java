db690cffecf38374779e83a69a65f9a414787d15
visualkhh <visualkhh@gmail.com>
2012-12-15 03:49:16 +0000

    h

--- test/src/nioRelayServerTest/NioClient1.java
+++ test/src/nioRelayServerTest/NioClient1.java
@@ -1,16 +1,13 @@
 package nioRelayServerTest;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 
 import khh.communication.tcp.nio.client.NioClient;
 import khh.communication.tcp.nio.worker.NioActionWorker;
 import khh.communication.tcp.nio.worker.NioWorker;
-import khh.communication.tcp.nio.worker.msg.FromToFormater;
 import khh.communication.tcp.nio.worker.msg.NioActionMsg;
 import khh.debug.LogK;
-import khh.util.ByteUtil;
 
 public class NioClient1 {
     public static void main(String[] args) throws IOException {
@@ -19,31 +16,31 @@ public class NioClient1 {
             LogK log = LogK.getInstance();
 			@Override
 			public NioActionMsg onReceiveAction(NioActionMsg msg, SelectionKey selectionKey) throws Exception{
-              FromToFormater fromto = new FromToFormater();
-              fromto.format(msg.get());
-              log.debug("Recv Msg :("+msg.getAction()+") " + fromto.getString());
+              log.debug("Recv Msg :("+msg.getAction()+") " + msg.getString());
               return msg;
 			}
 			
-			@Override
-			public NioActionMsg onSendAction(NioActionMsg msg, SelectionKey selectionKey) throws Exception{
-              FromToFormater fromto = new FromToFormater();
-              fromto.setFrom("client1");
-              fromto.setTo("client2");
-              //fromto.set(ByteUtil.toByteBuffer("From:Client1(9090) ~ To: Clinet2(801)   Msg: hi~client2"));
-              fromto.set("ClientRelayServer8011");
+            @Override
+            public NioActionMsg onSendAction(NioActionMsg msg, SelectionKey selectionKey) throws Exception {
+                if(msg==null){
+                    msg=new NioActionMsg();
+                }
+                msg.clear();
+                 msg.setAction(NioActionMsg.ACTION.GET_SERVERTIME.getValue());
+               // msg.setAction(NioActionMsg.ACTION.FROMTO.getValue());
+              //  msg.setAction(NioActionMsg.ACTION.GET_SERVERS.getValue());
+             //   msg.setAction(NioActionMsg.ACTION.GET_CLIENTS.getValue());
+                //msg.putParam(NioActionMsg.PARAM.FROM.getValue(), "client1");
+//                msg.putParam(NioActionMsg.PARAM.TO.getValue(), "client2");
+                // fromto.set(ByteUtil.toByteBuffer("From:Client1(9090) ~ To: Clinet2(801)   Msg: hi~client2"));
+//                msg.set("ClientRelayServer9090");
+                msg.setSuccess(true);
 
-              msg = new NioActionMsg();
-              msg.setAction(ACTION.FROMTO.getValue());
-              msg.setAction(ACTION.GET_SERVERS.getValue());
-              msg.setAction(ACTION.GET_CLIENTS.getValue());
-              msg.set(fromto);
-              msg.setSuccess(true);
-              sendNioActionMsg(msg);
-              Thread.sleep(3000);
-				
-              return msg;
-			}
+                sendNioActionMsg(msg);
+//                Thread.sleep(3000);
+
+                return msg;
+            }
 
         };
         NioClient client1 = new NioClient("127.0.0.1", 9090, client1Worker);
--- test/src/nioRelayServerTest/RelayServerTest.java
+++ test/src/nioRelayServerTest/RelayServerTest.java
@@ -15,3 +15,4 @@ public class RelayServerTest {
         f.start();
     }
 }
+
