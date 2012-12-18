b0e573ab4d9a9257cea633f24db1e60d9331e31c
visualkhh <visualkhh@gmail.com>
2012-12-18 02:19:48 +0000

    f

--- test/src/niotest/EchoNioClient.java
+++ test/src/niotest/EchoNioClient.java
@@ -3,12 +3,27 @@ import java.io.IOException;
 
 import khh.communication.tcp.nio.client.NioClient;
 public class EchoNioClient {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        NioClient c = new NioClient("127.0.0.1",801,new EchoNioClientWorker());
+    public static void main(String[] args) throws Exception {
+        final NioClient c = new NioClient("127.0.0.1",9595,EchoNioClientWorker.class);
 //        c.setConnectionTimeout(40);
-        c.connection();
         c.start();
         
-        Thread.sleep(5000);
+        
+        new Thread(){
+        	public void run() {
+        		int i=0;
+        		while(true){
+        			try{
+						sleep(1000);
+					}catch(InterruptedException e){
+					}
+        			c.pushTelegram(i+"");
+        			i++;
+        			if(i>5){
+        				return;
+        			}
+        		}
+        	};
+        }.start();
     }
 }
--- test/src/niotest/EchoNioClientWorker.java
+++ test/src/niotest/EchoNioClientWorker.java
@@ -4,19 +4,30 @@ import java.nio.channels.SelectionKey;
 import khh.communication.tcp.nio.worker.NioWorker;
 public class EchoNioClientWorker extends NioWorker {
     public EchoNioClientWorker() {
-        setFirestMode(MODE_FIREST_RW);
+//        setFirestMode(MODE_FIREST_W);
+        setFirestMode(MODE_DISABLE);
     }
     private int i=0;
     @Override
     public void execute(SelectionKey selectionKey) throws Exception {
+    	Object s=null;
+    	try{
+    		s = getNioCommunication().getTelegramQueue().pop();
+    	if(s!=null){
+    		write((String) s);
+    	}}catch (Exception e) {
+		}
+    	
+    	
+    	
         if(selectionKey.isReadable()){
             ByteBuffer b = ByteBuffer.allocate(1);
             read(b, 100);
             b.clear();
             System.out.println("Client Recv: "+(char)b.get());
         }
-        if(selectionKey.isWritable()){
-            Thread.sleep(2000);
+        if(selectionKey.isWritable()&&s==null){
+            Thread.sleep(5000);
             byte send = (byte)('a'+(i++));
             byte[] a = {send};
             System.out.println("Client Send: "+(char)send);
