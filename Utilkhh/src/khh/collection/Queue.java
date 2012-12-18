041b5cceb1e501c203f1c040e3ee142fd649c57c
visualkhh <visualkhh@gmail.com>
2012-12-18 02:14:17 +0000

    f

--- Utilkhh/src/khh/collection/Queue.java
+++ Utilkhh/src/khh/collection/Queue.java
@@ -3,41 +3,39 @@ package khh.collection;
 import java.util.NoSuchElementException;
 import java.util.PriorityQueue;
 
-public class Queue<T> {
-	private   java.util.Queue<T> queue = null;
-	
-	public Queue() {
-		synchronized (this) {
-//			queue =new LinkedList<T>();
-			queue =new PriorityQueue<T>();
+public class Queue<T>{
+	private java.util.Queue<T> queue = null;
+
+	public Queue(){
+		synchronized(this){
+			// queue =new LinkedList<T>();
+			queue = new PriorityQueue<T>();
 		}
-		
+
 	}
-	
+
 	synchronized public boolean push(T item){
 		synchronized(queue){
 			return queue.offer(item);
 		}
 	}
-	synchronized public T pop() throws NoSuchElementException
-	{
-		synchronized (queue) {
-			T item = queue.element();		
+
+	synchronized public T pop() throws NoSuchElementException{
+		synchronized(queue){
+			T item = queue.element();
 			queue.poll();
 			return item;
 		}
-		
+
 	}
-	synchronized public int size()
-	{
-		synchronized(queue){
-			return queue.size();
-		}
+
+	synchronized public int size(){
+		return queue.size();
 	}
-	
+
 	@Override
-	public String toString() {
+	public String toString(){
 		return queue.toString();
 	}
-	
+
 }
