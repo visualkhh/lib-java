786f6d1a892e2ecb128713fd1956345d44c44153
visualkhh@gmail.com <visualkhh@gmail.com@9f7e3b54-5923-cb47-458c-14d5223930df>
2012-12-21 09:06:29 +0000

    f

--- trunk/test/src/TTT.java
+++ trunk/test/src/TTT.java
@@ -8,8 +8,14 @@ import khh.url.util.URLUtil;
 public class TTT {
 	public static void main(String[] args) throws MalformedURLException, IOException {
 		//for (int i = 3725; i <99999 ; i++) {
-			FileUtil.writeFile(new File("G:\\viusalhhk\\list.txt"),"------",true);
+			//FileUtil.writeFile(new File("G:\\viusalhhk\\list.txt"),"------",true);
 			//System.out.println(i);
 		//}
+		String toDay ="20121220";
+			String startDate = "20121218";
+			String  endDate   = "20121219";
+		if((Integer.parseInt(startDate) <= Integer.parseInt(toDay)) && (Integer.parseInt(toDay) <= Integer.parseInt(endDate))){
+			System.out.println("탔어");
+		}
 	}
 }
