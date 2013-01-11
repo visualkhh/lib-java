ed3369154fa8234121b5d8e1ec9e16d535764107
visualkhh@gmail.com <visualkhh@gmail.com@9f7e3b54-5923-cb47-458c-14d5223930df>
2013-01-11 09:40:11 +0000

    f

---
+++ trunk/Utilkhh/src/khh/hui/part/input/HuiInput.java
@@ -0,0 +1,19 @@
+package khh.hui.part.input;
+
+import khh.hui.Hui;
+import khh.hui.interfaces.HuiInterface;
+
+/*
+ * 입력을 대신한다
+ */
+public abstract class HuiInput<T> implements HuiInterface<T>{
+	private Hui hui = null;
+	public Hui getHui(){
+		return hui;
+	}
+
+	public void setHui(Hui hui){
+		this.hui = hui;
+	}
+
+}
---
+++ trunk/Utilkhh/src/khh/hui/part/think/HuiThink.java
@@ -0,0 +1,19 @@
+package khh.hui.part.think;
+
+import khh.hui.Hui;
+import khh.hui.interfaces.HuiInterface;
+
+/*
+ * 생각을 대신한다
+ */
+public abstract class HuiThink<T> implements HuiInterface<T>{
+	private Hui hui = null;
+	public Hui getHui(){
+		return hui;
+	}
+
+	public void setHui(Hui hui){
+		this.hui = hui;
+	}
+
+}
