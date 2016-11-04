54a832881b58f7378b403be95be39dc8d6e66349
visualkhh <visulkhh@gmail.com>
2016-11-04 09:39:55 +0900

    java

--- Utilkhh/src/khh/db/util/ConnectionUtil.java
+++ Utilkhh/src/khh/db/util/ConnectionUtil.java
@@ -9,9 +9,12 @@ public class ConnectionUtil {
 
 	public final static int MYSQL=1; 
 	public final static int ORACLE=2; 
-	public final static String ORACLE_DAFAULT_PORT="1521"; 
+	public final static int HSQL=3; 
 	public final static String MYSQL_DAFAULT_PORT="3306"; 
 	
+	public static Connection getConnection(String drivepath, String jdbcConnectionURL, String ip, String port, String sid, String id, String passwd) throws ClassNotFoundException, SQLException{
+		return getConnection(drivepath, jdbcConnectionURL, id, passwd);
+	}
 	public static Connection getConnection(int dbtype, String ip, String port, String sid, String id, String passwd) throws ClassNotFoundException, SQLException{
 		String driverPath = getDriverPath(dbtype);
 		String jdbcConnectionURL = getJDBCConnectionURL(dbtype, ip, port, sid);
@@ -29,9 +32,11 @@ public class ConnectionUtil {
 		case MYSQL:
 			driverpath = "com.mysql.jdbc.Driver";
 			break;
-			
 		case ORACLE:
 			driverpath = "oracle.jdbc.driver.OracleDriver";
+			break;
+		case HSQL:
+			driverpath = "org.hsqldb.jdbcDriver";
 			break;
 		default:
 			driverpath=null;
@@ -46,9 +51,11 @@ public class ConnectionUtil {
 		case MYSQL:
 			driverUrl = "jdbc:mysql://"+ip+":"+port+"/"+sid+"?autoReconnect=true";
 			break;
-			
 		case ORACLE:
 			driverUrl = "jdbc:oracle:thin:@"+ip+":"+port+":"+sid;
+			break;
+		case HSQL:
+			driverUrl = "jdbc:hsqldb:hsql://"+ip+":"+port+"/"+sid;
 			break;
 		default:
 			driverUrl=null;
